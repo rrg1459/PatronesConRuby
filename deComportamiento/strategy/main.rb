68a33720fa525db252ae82058a5567f80c1c3c04
rafaDev <rrg1459@hotmail.com>
2024-02-28 10:17:10 -0400

    Agrega patron de diseño strategy

---
+++ deComportamiento/strategy/main.rb
@@ -0,0 +1,24 @@
+puts "\nStrategy"
+puts   "--------\n"
+##
+
+require_relative "impresor_html"
+require_relative "impresor_plano"
+require_relative "informe"
+
+puts
+puts "Informe HTML"
+puts
+
+informe = Informe.new(ImpresorHTML.new)
+informe.imprimir
+
+puts
+puts "Informe en texto plano"
+puts
+
+informe.impresor = ImpresorPlano.new
+informe.imprimir
+
+##
+puts "\nFin\n\n"
